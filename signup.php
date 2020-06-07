6217d74981a2419ee0722821158f1ebc7793ce98
Studio Phoenix <Foxstrock@gmail.com>
2020-06-07 21:32:43 +0200

    Added new tables

--- signup.php
+++ signup.php
@@ -1,2 +1,26 @@
 <?php
     include "header.php";
+?>
+
+<form action="#signup" method="post">
+    Nome:<br>
+    <input type="text" id="name" name="name"><br>
+    Cognome:<br>
+    <input type="text" id="surname" name="surname"><br>
+    E-Mail:<br>
+    <input type="text" id="email" name="email"><br>
+    Indirizzo:<br>
+    <input type="text" id="address" name="address"><br>
+    Data di Nascita:<br>
+    <input type="date" id="birthdate" name="birthdate"><br>
+    Password:<br>
+    <input type="password" id="password" name="password"><br>
+    Conferma password:<br>
+    <input type="password" id="passwordConf" name="passwordConf"><br>
+</form>
+
+<?php
+
+
+
+?>
