67a16d2392f123fe3c3cf108362533af86f6cd33
Studio Phoenix <Foxstrock@gmail.com>
2020-06-07 20:34:20 +0200

    Added new tables

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     if($_SERVER["REQUEST_METHOD"]=="GET"){
         ?>
         <form method="post" action="#login">
-            <input type="text" id="username" name="username"><br>
+            <input type="text" id="email" name="email"><br>
             <input type="password" id="password" name="password"><br>
             <input type="submit" id="login" name="login" value="Accedi!">
         </form>
@@ -11,11 +11,11 @@
     }elseif ($_SERVER['REQUEST_METHOD']=="POST"){
         if (isset($_POST['login'])){
             print_r($_POST);
-            $username = $_POST['username'];
+            $username = $_POST['email'];
             $password = md5($_POST['password']);
-            if($sql->query("SELECT * FROM users WHERE username='$username'")->num_rows > 0){
-                if($sql->query("SELECT * FROM users WHERE username='$username' AND passsword='$password'")->num_rows > 0){
-                    $user = $sql->query("SELECT * FROM users WHERE username='$username'")->fetch_array();
+            if($sql->query("SELECT * FROM users WHERE email='$username'")->num_rows > 0){
+                if($sql->query("SELECT * FROM users WHERE email='$username' AND passsword='$password'")->num_rows > 0){
+                    $user = $sql->query("SELECT * FROM users WHERE email='$username'")->fetch_array();
                     $_SESSION['user']['name'] = $user['name'];
                     $_SESSION['user']['surname'] = $user['surname'];
                     $_SESSION['user']['email'] = $user['email'];
