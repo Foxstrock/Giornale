202fe844b968b2c249aef4573abcfd36a2e9cd40
Studio Phoenix <Foxstrock@gmail.com>
2020-06-09 17:53:02 +0200

    funziona sta merda o no?

--- admin/manageUsers.php
+++ admin/manageUsers.php
@@ -1,1 +1,20 @@
 <?php
+    include "header.php";
+
+$num=$sql->query("Select * from users")->num_rows;
+
+$i=0;
+
+while($i<$num){
+
+    $name=mysqli_result($sql,$i,"name");
+    $surname=mysqli_result($sql,$i,"surname");
+    $email=mysqli_result($sql,$i,"email");
+    $address=mysqli_result($sql,$i,"address");
+    $birthdate=mysqli_result($sql,$i,"birthdate");
+
+    echo "$name $surname $email $address $birthdate<br>";
+
+
+    $i++;
+}
