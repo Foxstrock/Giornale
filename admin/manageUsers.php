0ce991badfd1b93c0a29400e28c0ad6d454e344c
Studio Phoenix <Foxstrock@gmail.com>
2020-06-10 10:24:00 +0200

    funziona sta merda o no?

--- admin/manageUsers.php
+++ admin/manageUsers.php
@@ -1,7 +1,7 @@
 <?php
     include "header.php";
 
-$result=$sql->query("SELECT * FROM users");
+$result=$sql->query("SELECT * FROM (users NATURAL JOIN usersLevel)NATURAL JOIN levels ");
 
 $row= $result->fetch_array(MYSQLI_NUM);
 
