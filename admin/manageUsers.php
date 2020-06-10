8ba04011fe4a2b2312ccb9ac0fad619a3d249258
Studio Phoenix <Foxstrock@gmail.com>
2020-06-10 10:21:59 +0200

    funziona sta merda o no?

--- admin/manageUsers.php
+++ admin/manageUsers.php
@@ -2,5 +2,10 @@
     include "header.php";
 
 $result=$sql->query("SELECT * FROM users");
-$num=$sql->query("COUNT * FROM users");
-echo $num;
+
+$row= $result->fetch_array(MYSQLI_NUM);
+
+while($row=$result->fetch_array(MYSQLI_NUM)){
+    print_r($row);
+    echo "<br>";
+}
