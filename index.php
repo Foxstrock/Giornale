e7abdb62ae09bf87c232a3c9a7145d753d59bc30
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 00:05:35 +0200

    funziona sta merda o no?

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
             <div class="col-lg">
                 <h2>Barcellona P.G. News</h2>
             </div>
-            <div class="col-sm">
+            <div class="col-md">
                 <a href="login.php">Login</a>
             </div>
-            <div class="col-sm">
+            <div class="col-md">
                 <a href="signup.php">Sign Up</a>
             </div>
             </div>
