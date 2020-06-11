a2b573eed422aee60d3a009145665a800f746966
Studio Phoenix <Foxstrock@gmail.com>
2020-06-11 20:17:27 +0200

    funziona sta merda o no?

--- readArticle.php
+++ readArticle.php
@@ -8,7 +8,13 @@
         if($articles->num_rows==0){
             echo "Non ci sono articoli, mettiti a scrivere pezzo di merda sennò ti bocciano!";
         }else {
-            echo "<table><tr><td>ID</td><td>Title</td><td>Photo</td><td>Category</td></tr>";
+            echo "<table>
+                        <tr>
+                            <td>ID</td>
+                            <td>Title</td>
+                            <td>Photo</td>
+                            <td>Category</td>
+                        </tr>";
             while ($article = $articles->fetch_assoc()) {
                 $articleID = $article['articleID'];
                 $title = $article['title'];
@@ -17,11 +23,19 @@
                 $tipoMimeFoto = $article['mimeType'];
                 $categoryName = $sql->query("SELECT name FROM categories WHERE categoryID='$categoryID'");
 
-                $data .= "<tr><td><a href='readArticle.php?articleID=$articleID'>$articleID</a></td><td>$title</td><td><img width='100' height='100' src='data: ".$tipoMimeFoto.";base64,".$attachment."'/></td><td>$categoryName</td></tr>";
-
+                $data .= "<tr>
+                            <td><a href='readArticle.php?articleID=$articleID'>$articleID</a></td>
+                            <td>$title</td>
+                            <td><img width='100' height='100' src='data: ".$tipoMimeFoto.";base64,".$attachment."'/></td>
+                            <td>$categoryName</td>
+                         </tr>";
+                $tmp[] = $article;
             }
             echo $data;
             echo "</table>";
+
+            echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+            print_r($tmp);
         }
     }else{
         $articleID = $_GET['articleID'];
