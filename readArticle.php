074f4538b4033dccf604786233131790c983b350
Studio Phoenix <Foxstrock@gmail.com>
2020-06-11 20:11:58 +0200

    funziona sta merda o no?

--- readArticle.php
+++ readArticle.php
@@ -5,23 +5,24 @@
     }
     if(!isset($_GET['articleID'])){
         $articles = $sql->query("SELECT * FROM articles");
-        while ($article = $articles->fetch_assoc()){
-            $articleID = $article['articleID'];
-            $title = $article['title'];
-            $subtitle = $article['subtitle'];
-            $text = $article['text'];
-            $attachment = $article['attachment'];
-            $createdAt = $article['createdAt'];
-            $creatorID = $article['creatorID'];
-            $lastEditorID = $article['lastEditorID'];
-            $lastEdited = $article['lastEdited'];
-            $categoryID = $article['categoryID'];
-            $categoryName = $sql->query("SELECT name FROM categories WHERE categoryID='$categoryID'");
+        if($articles->num_rows==0){
+            echo "Non ci sono articoli, mettiti a scrivere pezzo di merda sennò ti bocciano!";
+        }else {
+            echo "<table><tr><td>ID</td><td>Title</td><td>Photo</td><tr><td>Category</td></tr>";
+            while ($article = $articles->fetch_assoc()) {
+                $articleID = $article['articleID'];
+                $title = $article['title'];
+                $attachment = $article['attachment'];
+                $categoryID = $article['categoryID'];
+                $tipoMimeFoto = $article['mimeType'];
+                $categoryName = $sql->query("SELECT name FROM categories WHERE categoryID='$categoryID'");
 
-            print_r($article);
+                $data .= "<tr><td><a href='readArticle.php?articleID=$articleID'>$articleID</a></td><td>$title</td><td><img width='100' height='100' src='data: ".$tipoMimeFoto.";base64,".$attachment."'/></td><td>$categoryName</td></tr>";
 
+            }
+            echo $data;
+            echo "</table>";
         }
-
     }else{
         $articleID = $_GET['articleID'];
     }
