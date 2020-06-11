890b83c3cfa09ec0425c96b6ba96c074a1ad53cc
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 00:02:15 +0200

    funziona sta merda o no?

--- index.php
+++ index.php
@@ -3,7 +3,19 @@
 ?>
 
 <body>
-    <div>
-        <h2>Barcellona P.G. News</h2>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg">
+                <h2>Barcellona P.G. News</h2>
+            </div>
+            <div class="col-md">
+                <div class="col-sm">
+                    <a href="login.php">Login</a>
+                </div>
+                <div class="col-sm">
+                    <a href="signup.php">Sign Up</a>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
