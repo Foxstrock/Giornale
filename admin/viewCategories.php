a00950c8f81f643ae0f422c6c7269e35378a6b55
Studio Phoenix <Foxstrock@gmail.com>
2020-06-11 19:50:35 +0200

    funziona sta merda o no?

--- admin/viewCategories.php
+++ admin/viewCategories.php
@@ -1,13 +1,13 @@
 <?php
     $categories = $sql->query("SELECT * FROM categories");
-    while($category = $categories->fetch_assoc()){
-        $data .= "<tr><td>".$category['categoryID']."</td><td>".$category['name']."</td></tr>";
+    if($categories->num_rows==0){
+        $data = "Non ci sono categorie";
+    }else {
+        $data = "<table><tr><td>Category ID</td><td>Category Name</td></tr>";
+        while ($category = $categories->fetch_assoc()) {
+            $data .= "<tr><td>" . $category['categoryID'] . "</td><td>" . $category['name'] . "</td></tr>";
+        }
+        $data.= "</table>";
     }
+    echo $data;
 ?>
-<table>
-    <tr>
-        <td>Category ID</td>
-        <td>Category Name</td>
-    </tr>
-    <?php echo $data; ?>
-</table>
