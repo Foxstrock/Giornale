a54d11ab8d9b48539a47395517610e0b569c9901
Studio Phoenix <Foxstrock@gmail.com>
2020-06-11 19:58:22 +0200

    funziona sta merda o no?

--- admin/addArticle.php
+++ admin/addArticle.php
@@ -1,6 +1,8 @@
 <?php
     include "header.php";
-
+    if(!isset($_SESSION['user']['id'])){
+        header("location: ../login.php");
+    }
     if($_SERVER['REQUEST_METHOD']=='GET'){
         ?>
         <form action="#addArticle" method="post" enctype="multipart/form-data">
@@ -19,7 +21,11 @@
         $title=$_POST['title'];
         $subtitle=$_POST['subtitle'];
         $text=$_POST['text'];
-
+        $createdBy = $_SESSION['user']['id'];
+        $createdAt = date("Y-m-d H:i:s");
+        $lastEdited = $createdAt;
+        $lastEditedBy = $createdBy;
+        $category = $_POST['category'];
         $foto = base64_encode(file_get_contents($_FILES['foto']['tmp_name']));
         $tipoMimeFoto = mime_content_type($_FILES['foto']['tmp_name']);
         echo $foto.PHP_EOL;
