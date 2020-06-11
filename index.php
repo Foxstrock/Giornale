0e233a670ae54678e0470f259d3ac81fc27e20db
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 00:21:47 +0200

    funziona sta merda o no?

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
     include "header.php";
 ?>
 
-<body bgcolor="black">
+<body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg">
+            <div class="w-auto">
                 <h2>Barcellona P.G. News</h2>
             </div>
-            <div class="col-sm">
+            <div class="w-auto">
                 <a href="login.php">Login</a>
                 |
                 <a href="signup.php">Sign Up</a>
