0bdd84e0023b38484a312bda2b71dc5d465313ef
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 00:22:26 +0200

    funziona sta merda o no?

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <div class="w-auto">
                 <h2>Barcellona P.G. News</h2>
             </div>
-            <div class="w-auto">
+            <div class="w-25">
                 <a href="login.php">Login</a>
                 |
                 <a href="signup.php">Sign Up</a>
