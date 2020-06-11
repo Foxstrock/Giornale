7c3e932979b93a69f6c3d1e0fbb08bbaf391d3d4
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 00:25:15 +0200

    funziona sta merda o no?

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="w-auto">
+            <div class="ml-0">
                 <h2>Barcellona P.G. News</h2>
             </div>
-            <div class="w-25">
+            <div class="mr-0">
                 <a href="login.php">Login</a>
                 |
                 <a href="signup.php">Sign Up</a>
