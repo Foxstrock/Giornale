d6c05e08de37a980e7d9b0c1d276e969167f96f6
Studio Phoenix <Foxstrock@gmail.com>
2020-06-11 23:11:26 +0200

    funziona sta merda o no?

--- admin/deleteArticle.php
+++ admin/deleteArticle.php
@@ -1,1 +1,13 @@
 <?php
+    include "header.php";
+    if($_SESSION['user']['levelID'] < 3){
+        header("location: ../index.php");
+    }
+    if (isset($_GET['articleID'])){
+        $articleID = $_GET['articleID'];
+        $sql->query("DELETE FROM articles WHERE articleID = '$articleID'");
+        if($sql->error){
+            die($sql->error);
+        }
+        header("location: ../readArticle.php");
+    }
