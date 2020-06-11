e1011fbf5b84f7c533c0f4844855dcdd84c8c2e0
Daniele <daniele@danrizzo.dev>
2020-06-11 19:09:00 +0200

    create readArticle.php

--- readArticle.php
+++ readArticle.php
@@ -1,2 +1,27 @@
 <?php
     include "header.php";
+    if(!isset($_SESSION['user']['id'])){
+        header("location: index.php");
+    }
+    if(!isset($_GET['articleID'])){
+        $articles = $sql->query("SELECT * FROM articles");
+        while ($article = $articles->fetch_assoc()){
+            $articleID = $article['articleID'];
+            $title = $article['title'];
+            $subtitle = $article['subtitle'];
+            $text = $article['text'];
+            $attachment = $article['attachment'];
+            $createdAt = $article['createdAt'];
+            $creatorID = $article['creatorID'];
+            $lastEditorID = $article['lastEditorID'];
+            $lastEdited = $article['lastEdited'];
+            $categoryID = $article['categoryID'];
+            $categoryName = $sql->query("SELECT name FROM categories WHERE categoryID='$categoryID'");
+
+            print_r($article);
+
+        }
+
+    }else{
+        $articleID = $_GET['articleID'];
+    }
