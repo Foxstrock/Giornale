f61cc682186a02cbea296db4baced7e7f334cd22
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 02:40:27 +0200

    funziona sta merda o no?

--- confermaregistrazione.php
+++ confermaregistrazione.php
@@ -1,4 +1,14 @@
 <?php
     $email=_GET['email'];
 
-    $sql->(UPDATE )
+    $userID = $sql->query("SELECT * FROM users WHERE email='$email'")->fetch_array()['userID'];
+    if($sql->error){
+        die($sql->error);
+    }
+
+    $sql->query("INSERT INTO userStatus (userID, userStatus) VALUES ('$userID', 2)");
+    if($sql->error){
+        die($sql->error);
+    }
+
+    header("location: index.php");
