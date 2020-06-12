9e6c7aebd566daf7f75539a6b8969bdab99d7f49
drizzo0 <48352006+drizzo0@users.noreply.github.com>
2020-06-12 09:04:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,9 @@
                     if (isset($_SESSION['user'])) {
                         echo " <a href='logout.php'>Logout</a>";
                     }else {
-                        echo "<a href='login.php'>Login</a>";
+                        echo "<a href='login.php'>Login</a>|<a href='signup.php'>Sign Up</a>";
                     }
                 ?>
-                |
-                <a href="signup.php">Sign Up</a>
             </div>
             </div>
         </div>
