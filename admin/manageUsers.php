4f36b6e9b75b243ee34226eae8bd0911f131da02
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 02:45:52 +0200

    funziona sta merda o no?

--- admin/manageUsers.php
+++ admin/manageUsers.php
@@ -7,7 +7,7 @@
     if($_SERVER['REQUEST_METHOD'] == "GET"){
         if(!isset($_GET['action']) || $_GET['action'] == 'view'){
             //mostra utenti
-            echo "<table><tr><td>UserID</td><td>Name</td><td>Surname</td><td>Email</td><td>Address</td><td>Birthdate</td><td>Created At</td><td>Edit</td><td>Delete</td></tr>";
+            echo "<table><tr><td>UserID</td><td>Name</td><td>Surname</td><td>Email</td><td>Address</td><td>Birthdate</td><td>Created At</td><td>Status</td><td>Edit</td><td>Delete</td></tr>";
             $users = $sql->query("SELECT * FROM users");
             $status = $sql->query("SELECT * FROM status");
             while ($user = $users->fetch_assoc()){
