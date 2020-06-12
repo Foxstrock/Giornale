80c4ec54d04081c1653a7282ccb29a35e3e1aabc
Studio Phoenix <Foxstrock@gmail.com>
2020-06-12 20:13:23 +0200

    funziona sta merda o no?

--- admin/manageUsers.php
+++ admin/manageUsers.php
@@ -7,18 +7,55 @@
     if($_SERVER['REQUEST_METHOD'] == "GET"){
         if(!isset($_GET['action']) || $_GET['action'] == 'view'){
             //mostra utenti
-            echo "<table><tr><td>UserID</td><td>Name</td><td>Surname</td><td>Email</td><td>Address</td><td>Birthdate</td><td>Created At</td><td>Status</td><td>Edit</td><td>Delete</td></tr>";
+            echo "<table><tr><td>UserID</td><td>Name</td><td>Surname</td><td>Email</td><td>Address</td><td>Birthdate</td><td>Created At</td><td>Status</td><td>Scadenza Abbonamento</td><td>Edit</td><td>Delete</td></tr>";
             $users = $sql->query("SELECT * FROM users");
             while ($user = $users->fetch_assoc()){
                 $userID = $user['userID'];
+                //inizio controllo
+                $subscription = '';
+                $sub = $sql->query("SELECT * FROM subscriptions WHERE userID = '$userID'");
+                if($sub->num_rows == 0){
+                    $subscription = "Nessun abbonamento attivo";
+                }else{
+                    $subscription = $sub->fetch_array()['expirationDate'];
+                }
                 $statusID = $sql->query("SELECT * FROM userStatus WHERE userID = '$userID'")->fetch_array()['statusID'];
                 $statusName = $sql->query("SELECT * FROM status WHERE statusID = '$statusID'")->fetch_array()['name'];
-                $data .= "<tr><td>".$user['userID']."</td><td>".$user['name']."</td><td>".$user['surname']."</td><td>".$user['email']."</td><td>".$user['address']."</td><td>".$user['birthdate']."</td><td>".$user['createdAt']."</td><td>".$statusName."</td><td><a href='manageUsers.php?action=edit&userID=".$user['userID']."'>Edit</a></td><td><a href='manageUsers.php?action=delete&userID=".$user['userID']."'>Delete</a></td></tr>";
+                $data .= "<tr><td>".$user['userID']."</td><td>".$user['name']."</td><td>".$user['surname']."</td><td>".$user['email']."</td><td>".$user['address']."</td><td>".$user['birthdate']."</td><td>".$user['createdAt']."</td><td>".$statusName."</td><td>$subscription</td><td><a href='manageUsers.php?action=edit&userID=".$user['userID']."'>Edit</a></td><td><a href='manageUsers.php?action=delete&userID=".$user['userID']."'>Delete</a></td></tr>";
             }
             echo $data;
             echo "</table>";
         }elseif ($_GET['action'] == "edit"){
             //modifica
+            $userID = $_GET['userID'];
+            $user = $sql->query("SELECT * FROM users WHERE userID = '$userID'")->fetch_array();
+            $currentStatusID = $sql->query("SELECT statusID from userStatus where userID = '$userID'")->fetch_array()['statusID'];
+            $currentLevelID = $sql->query("SELECT levelID from userLevels where userID = '$userID'")->fetch_array()['levelID'];
+            $currentStatusName = $sql->query("SELECT name FROM status WHERE statusID = '$currentStatusID'")->fetch_array()['name'];
+            $currentLevelName = $sql->query("SELECT name FROM levels WHERE levelID = '$currentLevelID'")->fetch_array()['name'];
+            $currentStatus = "<option name=\"statusID$currentStatusID\">$currentStatusName</option>";
+            $currentLevel = "<option name=\"levelID$currentLevelID\">$currentLevelName</option>";
+            ?>
+            <form method="post" action="#">
+                <input type="text" name="name" id="name" readonly value="<?php echo $user['name']; ?>">
+                <input type="email" name="email" id="email" readonly value="<?php echo $user['email']; ?>">
+                <select name="status" id="staus">
+                    <?php echo $currentStatus; ?>
+                    <option disabled>-------</option>
+                    <option name="statusID1">Attesa</option>
+                    <option name="statusID2">Attivo</option>
+                    <option name="statusID3">Sospeso</option>
+                </select>
+                <select name="level" id="level">
+                    <?php echo $currentLevel; ?>
+                    <option disabled>-------</option>
+                    <option name="levelID1">Non abbonato</option>
+                    <option name="levelID2">Abbonato</option>
+                    <option name="levelID3">Redattore</option>
+                    <option name="levelID4">Direttore</option>
+                </select>
+            </form>
+            <?php
         }elseif ($_GET['action'] == "delete"){
             $userID = $_GET['userID'];
             $querys[] = "DELETE FROM userStatus WHERE userID = '$userID'";
--- header.php
+++ header.php
@@ -10,3 +10,34 @@
     @session_start();
     print_r($_SESSION);
     echo "<br><br>";
+
+    function changeUserStatusToSuspended($sql, $userID){
+        $sql->query("UPDATE userStatus SET statusID = 3 WHERE userID = '$userID'");
+        if($sql->error){
+            die($sql->error);
+        }
+    }
+
+    $subscriptions = $sql->query("select * from subscriptions");
+    if($subscriptions->num_rows>0){
+        while ($subscription = $subscriptions->fetch_assoc()){
+            $expiryDate = $subscription['expiryDate'];
+            $expiryArray = explode('-', explode(' ', $expiryDate));
+            $todayArray = explode('-', explode(' ', date('Y-m-d H:i:s'))[0]);
+            if($expiryArray[0] >= $todayArray[0]){
+                if($expiryArray[0] > $todayArray[0]){
+                    changeUserStatusToSuspended($sql, $subscription['userID']);
+                }else{
+                    if($expiryArray[1] >= $todayArray[1]){
+                        if($expiryArray[1] > $todayArray[1]){
+                            changeUserStatusToSuspended($sql, $subscription['userID']);
+                        }else{
+                            if($expiryArray[2] > $todayArray[2]){
+                                changeUserStatusToSuspended($sql, $subscription['userID']);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
