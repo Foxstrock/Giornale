7bd00370c6ec2d32a5de4979d7e85e5f734bdcf2
Studio Phoenix <Foxstrock@gmail.com>
2020-06-13 11:42:18 +0200

    fixato sto merda di coso

--- admin/managePaymentMethods.php
+++ admin/managePaymentMethods.php
@@ -5,9 +5,12 @@
     }
     if(!isset($_GET['action'])) {
         $paymentMethods = $sql->query("SELECT * FROM paymentMethods");
+        echo "<table><tr><td>Nome</td><td>Elimina</td></tr>";
         while ($paymentMethod = $paymentMethods->fetch_assoc()) {
             $data .= "<td>" . $paymentMethod['name'] . "</td><td><a href='managePaymentMethods.php?action=delete&methodName=" . $paymentMethod['name'] . "'>Elimina</td>";
         }
+        echo $data;
+        echo "</table>";
     }elseif ($_GET['action'] == "delete"){
         $paymentMethodName=$_GET['methodName'];
         $sql->query("DELETE FROM paymentMethods WHERE name='$paymentMethodName'");
